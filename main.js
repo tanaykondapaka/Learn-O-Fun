438da290d2bd1505262d13df941eb5b57e56f303
tanaykondapaka <87485235+tanaykondapaka@users.noreply.github.com>
2021-11-29 16:22:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+window.addEventListener("load",playAudio)
+function playAudio() {
+  document.getElementById("hi").play();
+  
+}
+window.addEventListener("load",playAudio)
+function playAudio() {
+  document.getElementById("start").play();
+  
+}
+var i = "0"
+
+function addA(){
+i="A" 
+localStorage.setItem("check",i)
+}
+function addC(){
+ 
+i = "C"
+localStorage.setItem("check",i)
+  }
+
+function check(){
+
+
+  
+window.location="question_page.html"
+  
+
+  
+ 
+  }
